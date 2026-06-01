3c2daf996ccfdcd0b4293bcbc6262d4d86c5995f
EduardoSig2210 <00012523@uca.edu.sv>
2026-06-01 17:30:30 -0600

    Feat: Subiendo proyecto parcial (Sin api funcional)

--- parcial/app/src/main/java/com/example/parcial2/pantalla/Result/ResultScreen.kt
+++ parcial/app/src/main/java/com/example/parcial2/pantalla/Result/ResultScreen.kt
@@ -1,7 +1,13 @@
 package com.example.parcial2.pantalla.Result
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -10,6 +16,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,6 +35,26 @@ fun ResultScreen() {
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             Text(text = "Resultado")
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color(0xFF8986B6)
+                ),
+
+                shape = RoundedCornerShape(12.dp),
+
+                modifier = Modifier
+                    .width(280.dp)
+                    .height(55.dp),
+
+                onClick = {
+
+                }
+            ) {
+                Text("Nuevo (Volver a votar)", color = Color.Black)
+            }
         }
+
     }
 }
